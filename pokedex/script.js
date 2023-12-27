d5c4b5098b523c0ae1a263782c0390b4ea50d770
Joel Silva <joel.a.silva1986@gmail.com>
2023-12-27 18:44:00 -0300

    Aula 4 - Adiciona busca de pokemon (o mistério da vírgula)

--- pokedex/script.js
+++ pokedex/script.js
@@ -1,16 +1,19 @@
 const listaTarefasHtml = document.getElementById('pokemons');
 
-const getPokemons = () => {
-    const API = fetch('https://pokeapi.co/api/v2/pokemon?limit=500');
-    API.then((response) => {
-        console.log(response);
-        return response.json()
-    }).then((data) => {
-        render(data.results);
-    })
+const getPokemons = async (name = '') => {
+    if(name != ''){
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const data = await response.json();
+        renderOne(data);
+    }else {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=500');
+        const data = await response.json();
+        renderAll(data.results);
+    }    
 }
 
-const render = (pokemons) => {
+const renderAll = (pokemons) => {
+    listaTarefasHtml.innerHTML = '';
     pokemons.map((pokemon, index) => {
         listaTarefasHtml.insertAdjacentHTML('beforeend', 
         `
@@ -20,6 +23,29 @@ const render = (pokemons) => {
             </li>
         `)
     })
+}
+
+const renderOne = (pokemon) => {
+    listaTarefasHtml.innerHTML = '';
+    listaTarefasHtml.insertAdjacentHTML('beforeend', 
+    `
+        <li class="pokemon-card">
+            <img src="https://raw.github.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${pokemon.id}.gif"/>
+            <h3>${pokemon.name}</h3>
+            <h4>Peso: ${pokemon.weight}</h4>
+            <p>Tipo:
+                ${pokemon.types.map(tipoPokemon => `
+                    ${tipoPokemon.type.name}
+                `)} 
+            </p>
+        </li>
+    `)
+}
+
+const searchPokemon = (event) => {
+    event.preventDefault();
+    const text = document.getElementById('buscarInput').value;
+    getPokemons(text);
 }
 
 getPokemons();
